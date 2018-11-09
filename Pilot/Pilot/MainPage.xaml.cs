c8f5277b1f945ac4ac5f979db5a11e08b8764660
Lukasz Narolski <narolskilukasz@gmail.com>
2018-11-09 22:38:05 +0100

    Poprawnie działające wyświetlanie klawiatury na Androidzie

--- Pilot/Pilot/MainPage.xaml.cs
+++ Pilot/Pilot/MainPage.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
 
 namespace Pilot
 {
@@ -13,6 +15,7 @@ namespace Pilot
         public MainPage()
         {
             InitializeComponent();
+            KeyboardRead.IsVisible = false;
         }
 
         private void Button_Shortcuts(object sender, EventArgs e)
@@ -22,7 +25,9 @@ namespace Pilot
 
         private void Show_keyboard(object sender, EventArgs e)
         {
-        X: KeyboardRead.Focus();
+            KeyboardRead.Unfocus();
+            Thread.Sleep(500);
+            KeyboardRead.Focus();
         }
 
         private void Button_Config(object sender, EventArgs e)
