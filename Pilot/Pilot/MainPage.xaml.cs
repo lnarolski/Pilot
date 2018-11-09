01b7f71e5a4bb1f42f6441b23b73eac6cad106d7
lnarolski <lukasz99981@gmail.com>
2018-11-09 21:09:12 +0100

    Rewizja z próbą wyświetlenia klawiatury ekranowej

--- Pilot/Pilot/MainPage.xaml.cs
+++ Pilot/Pilot/MainPage.xaml.cs
@@ -9,9 +9,25 @@ namespace Pilot
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Shortcuts(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Show_keyboard(object sender, EventArgs e)
+        {
+        X: KeyboardRead.Focus();
+        }
+
+        private void Button_Config(object sender, EventArgs e)
+        {
+
         }
     }
 }
