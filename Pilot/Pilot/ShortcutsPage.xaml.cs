8a572fd795ad87bf1194ee6a01a88585e583c623
lnarolski <lukasz99981@gmail.com>
2019-10-14 23:03:22 +0200

    Dalszy ciąg prac nad ekranem skrótów

--- Pilot/Pilot/ShortcutsPage.xaml.cs
+++ Pilot/Pilot/ShortcutsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,24 +10,48 @@ using Xamarin.Forms.Xaml;
 
 namespace Pilot
 {
+    public struct ShortcutCell
+    {
+        public string Image { get; set; }
+        public string Text { get; set; }
+        public string WWWAddress { get; set; }
+    }
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ShortcutsPage : ContentPage
 	{
-		public ShortcutsPage ()
+        public static ObservableCollection<ShortcutCell> Shortcuts { get; set; }
+        public ShortcutsPage ()
 		{
 			InitializeComponent ();
 
-            ShortcutsButtonsGrid.Children.Add(new Button()
-            {
-                Text = "Dodaj nowy skrót",
-                Image = "Pilot.img.plus.png",
-                ContentLayout = new Button.ButtonContentLayout(Button.ButtonContentLayout.ImagePosition.Left, 20),
-            });
+            Shortcuts = new ObservableCollection<ShortcutCell>();
+
+            RefreshShortcutsList();
 		}
+
+        private void RefreshShortcutsList()
+        {
+            Shortcuts.Clear();
+
+            Shortcuts.Add(new ShortcutCell()
+            {
+                Image = "plus.png",
+                Text = "Dodaj nowy skrót",
+                WWWAddress = "",
+            });
+
+            ShortcutsListView.ItemsSource = null;
+            ShortcutsListView.ItemsSource = Shortcuts; //Podpięcie nowej listy skrótów do kontrolki ListView
+        }
 
         private void BackButton_Clicked(object sender, EventArgs e)
         {
             Navigation.PopModalAsync();
+        }
+
+        private void ShortcutsListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
         }
     }
 }
