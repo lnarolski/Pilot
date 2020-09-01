c8148efb16acceed9b14ae49da59112d5e148eea
lnarolski <narolskilukasz+github@gmail.com>
2020-09-01 22:00:39 +0200

    Dodanie automatycznego ponownego łączenia z serwerem po maksymalizacji aplikacji

--- Pilot/Pilot/App.xaml.cs
+++ Pilot/Pilot/App.xaml.cs
@@ -22,11 +22,15 @@ namespace Pilot
         protected override void OnSleep()
         {
             // Handle when your app sleeps
+            if (ConnectionClass.connected)
+                ConnectionClass.Disconnect();
         }
 
         protected override void OnResume()
         {
             // Handle when your app resumes
+            if (!ConnectionClass.connected && ConnectionClass.ipAddress != "")
+                ConnectionClass.Connect(ConnectionClass.ipAddress, ConnectionClass.port.ToString(), ConnectionClass.password);
         }
     }
 }
