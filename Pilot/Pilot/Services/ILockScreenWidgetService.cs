ab14e339cc6a32d92c35e03714d107332b578993
lnarolski <narolskilukasz+github@gmail.com>
2021-06-20 00:25:03 +0200

    Working on lockscreen widget

---
+++ Pilot/Pilot/Services/ILockScreenWidgetService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pilot
+{
+    public interface ILockScreenWidgetService
+    {
+        void CreateLockScreenWidget();
+        void RemoveLockScreenWidget();
+        void UpdateLockeScreenWidget();
+        void SendCommandToServer();
+    }
+}
