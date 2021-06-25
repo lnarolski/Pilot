f99b7d6f26eb15056f301b9bad7aa6b1ca1fb2a8
lnarolski <narolskilukasz+github@gmail.com>
2021-06-25 18:21:04 +0200

    Working on lockscreen widget

--- Pilot/Pilot.Android/Services/WidgetService.cs
+++ Pilot/Pilot.Android/Services/WidgetService.cs
@@ -35,41 +35,47 @@ namespace Pilot.Droid.Services
         public const int messageId = 0;
 
         public static bool initialised = false;
+        private MediaSessionCompat mediaSessionCompat;
+        private PlaybackStateCompat playbackStateCompat;
 
-        //private void CreateMediaSession()
-        //{
-        //    mediaSessionCompat = new MediaSessionCompat(context, "MediaSession");
+        private void CreateMediaSession()
+        {
+            mediaSessionCompat = new MediaSessionCompat(context, "MediaSession");
 
-        //    playbackStateCompat = new PlaybackStateCompat.Builder().SetActions(PlaybackStateCompat.ActionPlay | PlaybackStateCompat.ActionPause | PlaybackStateCompat.ActionPlayPause | PlaybackStateCompat.ActionSkipToNext | PlaybackStateCompat.ActionSkipToPrevious).Build();
-        //    mediaSessionCompat.SetMediaButtonReceiver(null);
-        //    mediaSessionCompat.SetPlaybackState(playbackStateCompat);
-        //    mediaSessionCompat.SetCallback(null);
+            playbackStateCompat = new PlaybackStateCompat.Builder().SetActions(PlaybackStateCompat.ActionPlay | PlaybackStateCompat.ActionPause | PlaybackStateCompat.ActionPlayPause | PlaybackStateCompat.ActionSkipToNext | PlaybackStateCompat.ActionSkipToPrevious).Build();
+            mediaSessionCompat.SetMediaButtonReceiver(null);
+            mediaSessionCompat.SetPlaybackState(playbackStateCompat);
+            mediaSessionCompat.SetCallback(null);
 
-        //    mediaSessionCompat.Active = true;
-        //}
+            mediaSessionCompat.Active = true;
+        }
 
         public void CreateWidget()
         {
             if (initialised)
                 return;
 
-            //CreateMediaSession();
             context = AndroidApp.Context;
+
+            CreateMediaSession();
 
             CreateNotificationChannel();
 
-            Intent intent = new Intent(context, typeof(MainActivity));
-            intent.PutExtra(TitleKey, TitleKey);
-            intent.PutExtra(MessageKey, MessageKey);
+            NotificationCompat.Action playPauseAction = new NotificationCompat.Action(Resource.Mipmap.icon, "default", MediaButtonReceiver.BuildMediaButtonPendingIntent(context, PlaybackStateCompat.ActionPlayPause));
 
-            PendingIntent pendingIntent = PendingIntent.GetActivity(context, pendingIntentId, intent, PendingIntentFlags.OneShot);
-
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
-                .SetContentIntent(pendingIntent)
-                .SetContentTitle(TitleKey)
-                .SetContentText(MessageKey)
+            AndroidX.Core.App.NotificationCompat.Builder builder = new AndroidX.Core.App.NotificationCompat.Builder(context, channelId);
+            builder.SetVisibility(NotificationCompat.VisibilityPublic)
                 .SetSmallIcon(Resource.Mipmap.icon)
-                .SetDefaults((int)NotificationDefaults.Sound | (int)NotificationDefaults.Vibrate);
+                // Add media control buttons that invoke intents in your media service
+                .AddAction(Resource.Drawable.ic_skip_previous, "Previous", MediaButtonReceiver.BuildMediaButtonPendingIntent(context, PlaybackStateCompat.ActionSkipToPrevious)) // #0
+                .AddAction(Resource.Drawable.ic_play_circle_outline, "Pause", MediaButtonReceiver.BuildMediaButtonPendingIntent(context, PlaybackStateCompat.ActionPlayPause))  // #1
+                .AddAction(Resource.Drawable.ic_skip_next, "Next", MediaButtonReceiver.BuildMediaButtonPendingIntent(context, PlaybackStateCompat.ActionSkipToNext))     // #2
+                .SetContentTitle("Wonderful music")
+                .SetContentText("My Awesome Band")
+                .SetStyle(new AndroidX.Media.App.NotificationCompat.MediaStyle().SetShowActionsInCompactView(1 /* #1: pause button */).SetMediaSession(mediaSessionCompat.SessionToken))
+                .SetOngoing(true)
+                .SetNotificationSilent()
+                .SetVibrate(new long[] { 0L });
 
             Notification notification = builder.Build();
             notificationManager.Notify(messageId, notification);
