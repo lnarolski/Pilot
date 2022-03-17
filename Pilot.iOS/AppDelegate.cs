8633186a0b527dd0766be58fa2159bd1eae8bb82
lnarolski <narolskilukasz+github@gmail.com>
2022-03-17 23:55:38 +0100

    Added iOS project

---
+++ Pilot.iOS/AppDelegate.cs
@@ -0,0 +1,23 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+namespace Pilot.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            Xamarin.Forms.Forms.Init();
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(application, launchOptions);
+        }
+    }
+}
+
+
