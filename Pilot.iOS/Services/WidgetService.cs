4c76343d4cc0f98c041e04e6fe0cd06b8d8bb600
lnarolski <narolskilukasz+github@gmail.com>
2022-03-22 23:42:08 +0100

    iOS bug fixes

---
+++ Pilot.iOS/Services/WidgetService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Pilot.iOS.Services.WidgetService))]
+namespace Pilot.iOS.Services
+{
+    public class WidgetService : IWidgetService
+    {
+        public WidgetService()
+        {
+        }
+
+        public void CreateWidget()
+        {
+            
+        }
+
+        public void RemoveWidget()
+        {
+            
+        }
+
+        public void SendCommandToServer(CommandsFromClient command)
+        {
+            
+        }
+
+        public void UpdateWidget(string artist, string title, byte[] thumbnail)
+        {
+            
+        }
+    }
+}
